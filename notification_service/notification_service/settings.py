becd6b0a4b82aa95ed683a52a49e92461a952c45
Roman <51775332+Povarenskiy@users.noreply.github.com>
2022-12-05 11:04:43 +0300

    Update settings.py
    Доступ всем хостам

--- notification_service/notification_service/settings.py
+++ notification_service/notification_service/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-)9=&==brrjgr7xfvp(o0(8k*bwoon3z&d#-%4x_dw+l89r3-kw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
